87c0acb76595b9eba21377fd840b67c5127b1074
Ukatu Michael <ukatuleonaldino@gmail.com>
2022-09-13 07:31:09 -0700

    pritn alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ *  *  *  * print_alphabet_x10 - function that prints 10 times alphabet
+ *
+ *   *
+ *
+ *    *   *
+ *
+ *     *
+ *
+ *      *    *   *
+ *
+ *       *
+ *
+ *        *     *
+ *
+ *         *
+ *
+ *          *      *    * Return: Always 0 (Success)
+ *
+ *           *
+ *
+ *            *       *
+ *
+ *             *
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 1;
+
+	while (num < 11)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,4 @@
 #include <stdio.h>
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
