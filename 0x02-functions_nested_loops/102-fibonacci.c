c8297a721352ae693faad046fd9deda14381440d
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-13 15:54:04 -0300

    Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.  The numbers must be separated by comma, followed by a space ,  You are allowed to use the standard library

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints even  number < 4,000,000
+ * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	int a = 0, b = 1, next = 0;
-	int sum = 0;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-	while (next < 4000000)
+	while (i < 50)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-		if (next % 2 == 0)
-		sum += next;
-		}
-	printf("%i\n", sum);
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+			}
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
