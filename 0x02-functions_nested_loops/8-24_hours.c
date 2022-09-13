ca5d3c125b4947075b869366ea5d152a8da3ec9b
Ukatu Michael <ukatuleonaldino@gmail.com>
2022-09-13 10:45:15 -0700

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ *  *  *  * jack_bauer - Write a function that prints every minute of the day
+ *
+ *   *
+ *
+ *    *   *
+ *
+ *     *
+ *
+ *      *    *   *
+ *      *       *
+ *
+ *        *     *
+ *
+ *         *
+ *
+ *          *      *    * Return: Always 0 (Success)
+ *
+ *           *
+ *
+ *            *       *
+ *
+ *             *
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
