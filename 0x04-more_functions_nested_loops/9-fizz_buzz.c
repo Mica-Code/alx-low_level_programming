4cebcd5193d4e75ac4cbbfec6707215b527b8e13
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-15 14:06:06 -0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point, prints number from 1 t0 100
+ * but for multiples of 3, print Fiss
+ * and print Buzz for multiples of 5
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
