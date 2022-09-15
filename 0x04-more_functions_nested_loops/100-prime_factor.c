ee823d83b14b49cf13773f6b2b79f957604bd657
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-15 15:46:01 -0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-int main() {
-   long int n;
-   n=612852475143;
-   long int div=2, ans = 0, maxFact;
-   while(n!=0) {
-      if(n % div !=0)
-         div = div + 1;
-      else {
-         maxFact = n;
-         n = n / div;
-         if(n == 1) {
-            printf("%d is the largest prime factor !",maxFact);
-            ans = 1;
-            break;
-         }
-      }
-   }
-   return 0;
+#include <math.h>
+/**
+ * main - prime numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+unsigned int long n = 612852475143, a = (int) sqrt(n);
+
+while (1)
+{
+
+	if (n % a == 0)
+	{
+		printf("%lu \n", n / a);
+		break;
+	}
+	a--;
+
+}
+
+return (0);
 }
