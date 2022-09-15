805f8609243a8afe72ee984776c89755970ae502
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-15 12:36:14 -0300

    correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,9 @@ void print_most_numbers(void)
 	for (a = 0; a <= 9; a++)
 	{
 		if (a == 2 || a == 4)
-    {
-      a++;
-    }
+    		{
+			continue;
+    		}
 		_putchar(a);
 	}
 	_putchar('\n');
