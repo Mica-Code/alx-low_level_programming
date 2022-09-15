37c5b54fe3c97f4803fb7d415df110829c0e48cb
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-15 14:46:06 -0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,6 +16,8 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
