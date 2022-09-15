702513403900631829c2e64f7a6500dc949d361a
Ukatu Michael <ukatuleonaldino@gmail.com>
2022-09-15 09:55:17 -0700

    committing task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,28 +7,29 @@
 
 void print_diagonal(int n)
 {
-  if (n != 0)
-  {
-    int i;
-    int j;
-    
-    for (i = 0; i < n; i++)
-    {
-      for (j = 0; j < i+1; j++)
-      {
-        if (i == j)
-        {
-          _putchar('\\');
-        }
-        else
-        {
-          _putchar(' ');
-        }
-      } 
-      _putchar('\n');
-    }
-    else
-    {
-      _putchar('\n');
-    }
+	if (n != 0)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i + 1; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
 }
