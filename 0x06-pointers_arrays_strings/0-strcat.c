1470451b33cfb08d6a1f10418f61cd916ecf3857
Ukatu Michael Ikechukwu <56010654+Mica-Code@users.noreply.github.com>
2022-09-22 17:22:01 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - function to concatenate two strings and return a new string
- * with n elements of the appended string
- *
- * @dest: string 1
- * @src: string 2
- * @n: number of chars to include from appended string
- * Return: char pointer
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i = 0, j = 0;
-	char *p;
 
-	while (*(dest + i) != '\0')
-		i++;
-	while (!(*(src + j) == '\0' || j == n))
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		*(dest + i) = *(src + j);
 		i++;
+		}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		j++;
-	}
-	*(dest + i) = '\0';
-	p = dest;
-	return (p);
+		i++;
+		}
+
+	dest[i] = '\0';
+	return (dest);
 }
+
